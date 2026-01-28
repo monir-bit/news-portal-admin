47e7de37c6adf49bc4a9a79ed1fefb327c6b8a9b
monir-bit <monir@dainikanandabazar.com>
2026-01-28 20:05:49 +0600

    feat: add news details endpoint and resource transformation

--- app/Http/Controllers/Api/NewsController.php
+++ app/Http/Controllers/Api/NewsController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Http\Resources\Api\NewsDetailsResource;
+use App\Http\Resources\Api\NewsListResource;
+use App\Models\Category;
 use App\Models\News;
 use Illuminate\Http\Request;
 
@@ -18,8 +20,26 @@ class NewsController extends Controller
                 'tags'
             ])->firstOrFail();
 
-
-
         return NewsDetailsResource::make($news);
     }
+
+    public function newsByCategory($slug)
+    {
+        $category = Category::where('slug', $slug)->where('visible', true)->firstOrFail();
+        $newsList = News::where('category_id', $category->id)
+            ->with(['category.parentRecursive'])
+            ->orderBy('created_at', 'DESC')
+            ->limit(15)
+            ->get();
+
+        return [
+            'category' => [
+                'name' => $category->name,
+                'slug' => $category->slug,
+            ],
+            'news_list' => NewsListResource::collection($newsList),
+        ];
+
+    }
+
 }
--- routes/api.php
+++ routes/api.php
@@ -7,3 +7,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/common', [\App\Http\Controllers\Api\CommonController::class, 'common']);
 Route::get('/home', [\App\Http\Controllers\Api\HomeController::class, 'homeInitial']);
 Route::get('/news-details/{slug}', [\App\Http\Controllers\Api\NewsController::class, 'newsDetails']);
+Route::get('/news-by-category/{slug}', [\App\Http\Controllers\Api\NewsController::class, 'newsByCategory']);
