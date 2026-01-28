330d52554b8d0966ca7e96959f9dd66500dace8a
monir-bit <monir@dainikanandabazar.com>
2026-01-28 20:40:41 +0600

    feat: restructure news retrieval by category to include lead, sub-lead, and other news

--- app/Http/Controllers/Api/NewsController.php
+++ app/Http/Controllers/Api/NewsController.php
@@ -26,18 +26,24 @@ class NewsController extends Controller
     public function newsByCategory($slug)
     {
         $category = Category::where('slug', $slug)->where('visible', true)->firstOrFail();
-        $newsList = News::where('category_id', $category->id)
+        $query = News::where('category_id', $category->id)
             ->with(['category.parentRecursive'])
-            ->orderBy('created_at', 'DESC')
-            ->limit(15)
-            ->get();
+            ->orderBy('created_at', 'DESC');
+
+        $leadNews = clone $query->skip(0)->take(5)->get();
+        $subLeadNews = clone $query->skip(5)->take(2)->get();
+        $othersNews = clone $query->skip(7)->take(12)->get();
 
         return [
             'category' => [
                 'name' => $category->name,
                 'slug' => $category->slug,
             ],
-            'news_list' => NewsListResource::collection($newsList),
+            'news_list' => [
+                'lead_news' => NewsListResource::collection($leadNews),
+                'sub_lead_news' => NewsListResource::collection($subLeadNews),
+                'others_news' => NewsListResource::collection($othersNews),
+            ],
         ];
 
     }
